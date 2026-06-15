f45ae3a8d48ca12a5551f9524f5c1131a205b6c4
Thomas Taylor <thomas.taylor@kineo.com>
2026-06-15 12:03:56 +0100

    New: Add a projects filter for courses by user group

--- ui/plugin.js
+++ ui/plugin.js
@@ -5,9 +5,22 @@
  * @module ui/plugin
  */
 import UserGroupsBadge from './UserGroupsBadge.jsx'
+import Icons from '@adapt-ui/utils/icons'
 
 export default {
   collectionBadges: [
     { id: 'usergroups', Component: UserGroupsBadge }
+  ],
+  collectionFilters: [
+    // filter the projects dashboard by a course's shared groups. userGroups is a
+    // real course field, so this is a normal `{ userGroups: { $in: [...] } }`
+    // query; options are sourced live from the usergroups collection.
+    {
+      collection: 'content',
+      field: 'userGroups',
+      label: 'Groups',
+      icon: Icons.UserGroup,
+      optionsApi: { apiRoot: 'usergroups', labelField: 'displayName' }
+    }
   ]
 }
